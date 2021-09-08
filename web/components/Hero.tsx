db017442b10af903c6e50b67b51e440c214217ff
Andrei Barabas <andreibarabas@gmail.com>
2021-09-09 00:21:47 +0300

    apply optimization

--- web/components/Hero.tsx
+++ web/components/Hero.tsx
@@ -1,22 +1,31 @@
+import Image from "next/image";
+
 export const Hero: React.FC = () => {
   return (
-    <div className="hero-background">
+    <div>
+      <div className="background-container">
+        <Image
+          priority={true}
+          src={BackgroundImage}
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
       <div className="container col-xxl-8 px-4 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6">
             {/* ----------------------- */}
             <img
               className="d-block mx-lg-auto img-fluid"
-              src="/images/woman.png"
-              alt="image"
-              width="700"
-              height="500"
+              src={WomanPicture}
+              layout="responsive"
             />
             {/* ----------------------- */}
           </div>
           <div className="col-lg-6">
             <h1 className="display-5 fw-bold lh-1 mb-3">
-              Before applying optimization
+              After applying optimization
             </h1>
             <p className="lead">
               This example is ment to showcase tips & tricks on how to optimize
