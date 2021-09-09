510e5bf16a733e39736aff587803b3a79df201fe
Andrei Barabas <andreibarabas@gmail.com>
2021-09-09 10:05:29 +0300

    typo

--- web/components/Hero.tsx
+++ web/components/Hero.tsx
@@ -17,13 +17,13 @@ export const Hero: React.FC = () => {
               Before applying optimization
             </h1>
             <p className="lead">
-              This example is ment to showcase tips&trics on how to optimize
+              This example is ment to showcase tips & tricks on how to optimize
               responsive images and what is the impact on the page loading
               speed.
             </p>
             <div className="d-grid gap-2 d-md-flex justify-content-md-start">
               <a
-                href="https://github.com/andreibarabas/achieving-100ms/blob/S01.E01-before/README"
+                href="https://github.com/andreibarabas/achieving-100ms/blob/S01.E01-before/README.md"
                 className="btn btn-primary btn-lg px-4 me-md-2"
               >
                 Learn more
